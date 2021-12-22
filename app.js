3943ae2527f6cdfb82aea4ed689507d90b4ab2e2
nnnlog <sorisem4106@naver.com>
2021-12-22 15:18:19 +0900

    fix error

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const loadData = async () => {
 };
 
 loadData().then(data => {
-	require("node-schedule").scheduleJob("*/10 * * * *", () => data = loadData());
+	require("node-schedule").scheduleJob("*/10 * * * *", async () => data = await loadData());
 
 	app.use(cors({origin: "nlog.dev"}));
 	app.get("/", (req, res) => {
